f6e721596942e7f69dd37525d4b86a2fd61e0051
Luis Alcaras <luisalcarasr@gmail.com>
2022-03-13 14:09:00 -0600

    👋 Shell welcome message has been removed

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
+set fish_greeting
 alias dotctl='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias pacman='sudo pacman'
 alias ls='exa'
