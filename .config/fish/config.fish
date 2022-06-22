06fe12d88c402e5362186ebdbf3a52f63f18e9f7
Luis Alcaras <luisalcarasr@gmail.com>
2022-06-21 19:43:11 -0500

    📦 HTB command has been added.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,4 +3,6 @@ if status is-interactive
 end
 set fish_greeting
 alias dotctl='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-alias ls='exa -la --git'
+alias ls='exa --git'
+alias py=python
+alias htb='nmcli c up HTB && sudo route del -net default gw 10.10.14.1 netmask 0.0.0.0 dev tun0'
