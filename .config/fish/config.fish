c1fd10796ad77586a2e3429ca682a822b080859f
Luis Alcaras <luisalcarasr@gmail.com>
2022-09-01 00:07:41 -0500

    😊 Some aliases have been added.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,9 @@ if status is-interactive
 end
 set fish_greeting
 alias dotctl='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-alias ls='exa --git'
+alias ls='exa'
+alias ll='exa -l'
+alias la='exa -la'
 alias py=python
 alias htb='nmcli c up HTB && sudo route del -net default gw 10.10.14.1 netmask 0.0.0.0 dev tun0'
+alias gg=lazygit
