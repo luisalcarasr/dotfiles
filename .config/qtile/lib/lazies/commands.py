e12e9a5f028a15d9e91579f2c53ba2abd91f606f
Luis Alcaras <luisalcarasr@gmail.com>
2023-05-11 14:08:01 -0600

    🐍 Qtile configuration has been removed.

--- .config/qtile/lib/lazies/commands.py
+++
@@ -1,6 +0,0 @@
-from libqtile.lazy import lazy
-
-@lazy.function
-def poweroff(qtile):
-    qtile.cmd_spawn('poweroff')
-
--- .config/qtile/utils/queries.py
+++
@@ -1,5 +0,0 @@
-from os import environ, path 
-
-can_control_brightness = path.exists("/sys/class/backlight/intel_backlight/brightness")
-has_batery = path.exists("/sys/class/power_supply/BAT0")
-
