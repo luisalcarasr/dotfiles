a21ccb3fd6862abd78f68e1d73726220a05062ec
Luis Alcaras <luisalcarasr@gmail.com>
2023-08-28 20:10:40 -0600

    🪟 Auto layout has been added.

--- .config/awesome/layouts/init.lua
+++ .config/awesome/layouts/init.lua
@@ -1,13 +1,48 @@
 local awful = require("awful")
 
+local vertical = function(p, context, layout_config)
+    local wa = p.workarea
+    local cls = p.clients
+
+    local height = wa.height / #cls
+    local y = wa.y
+
+    for _, c in ipairs(cls) do
+        local g = {
+            x = wa.x + p.useless_gap,
+            y = y + p.useless_gap,
+            width = wa.width - p.useless_gap * 2,
+            height = height - p.useless_gap * 2,
+        }
+        c:geometry(g)
+        y = y + g.height + p.useless_gap * 2
+    end
+end
+
+local layout = {
+    auto = {
+        name = awful.layout.suit.tile.name,
+        skip_gap = awful.layout.suit.tile.skip_gap,
+        arrange = function(p, context, layout_config)
+            local wa = p.workarea
+            if wa.height > wa.width then
+                vertical(p, context, layout_config)
+            else
+                awful.layout.suit.tile.arrange(p, context, layout_config)
+            end
+        end,
+    },
+}
+
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-    -- awful.layout.suit.floating,
-    awful.layout.suit.tile,
+    layout.auto,
+    -- awful.layout.suit.tile,
     -- awful.layout.suit.tile.left,
     -- awful.layout.suit.tile.bottom,
     -- awful.layout.suit.tile.top,
-    awful.layout.suit.fair,
+    -- awful.layout.suit.floating,
+    -- awful.layout.suit.fair,
     -- awful.layout.suit.fair.horizontal,
     -- awful.layout.suit.spiral,
     -- awful.layout.suit.spiral.dwindle,
