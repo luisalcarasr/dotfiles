d12acc53e66540770922ee8ac668bb48c9860791
Luis Alcaras <luisalcarasr@gmail.com>
2023-08-29 19:26:09 -0600

    🪟 Force fullscreen in games.

--- .config/awesome/rules/init.lua
+++ .config/awesome/rules/init.lua
@@ -15,43 +15,60 @@ awful.rules.rules = {
             keys = clientkeys,
             buttons = clientbuttons,
             screen = awful.screen.preferred,
-            placement = awful.placement.centered
-        }
+            placement = awful.placement.centered,
+        },
     }, -- Floating clients.
     {
         rule_any = {
             instance = {
                 "DTA", -- Firefox addon DownThemAll.
                 "copyq", -- Includes session name in class.
-                "pinentry"
+                "pinentry",
             },
             class = {
-                "Arandr", "Blueman-manager", "Gpick", "Kruler", "MessageWin", -- kalarm.
-                "Sxiv", "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
-                "Wpa_gui", "veromix", "xtightvncviewer"
+                "Arandr",
+                "Blueman-manager",
+                "Gpick",
+                "Kruler",
+                "MessageWin", -- kalarm.
+                "Sxiv",
+                "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
+                "Wpa_gui",
+                "veromix",
+                "xtightvncviewer",
             },
 
             -- Note that the name property shown in xprop might be set slightly after creation of the client
             -- and the name shown there might not match defined rules here.
             name = {
-                "Event Tester" -- xev.
+                "Event Tester", -- xev.
             },
             role = {
                 "AlarmWindow", -- Thunderbird's calendar.
                 "ConfigManager", -- Thunderbird's about:config.
-                "pop-up" -- e.g. Google Chrome's (detached) Developer Tools.
-            }
+                "pop-up", -- e.g. Google Chrome's (detached) Developer Tools.
+            },
         },
-        properties = {floating = true}
+        properties = { floating = true },
     }, -- Add titlebars to normal clients and dialogs
     {
-        rule_any = {type = {"normal", "dialog"}},
-        properties = {titlebars_enabled = true}
-    } -- Always center floating clients
+        rule_any = { type = { "normal", "dialog" } },
+        properties = { titlebars_enabled = true },
+    }, -- Always center floating clients
     -- Set Firefox to always map on the tag named "2" on screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { screen = 1, tag = "2" } },
     -- Spawn floating clients centered
-
+    -- Force fullscreen
+    {
+        rule = {
+            fullscreen = true,
+        },
+        properties = {
+            floating = true,
+            ontop = true,
+            fullscreen = true,
+        },
+    },
 }
 -- }}}
