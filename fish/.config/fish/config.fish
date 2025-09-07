de75e82eec81468cf59e6c224b8d0eda6d0877ed
Luis Alcaras <l.alcaras@f5.com>
2025-09-06 21:01:08 -0700

    🐟 Some modifications have been made in PATH.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,17 @@
 set fish_greeting
 
+# Aliases
 alias ls='exa --group-directories-first'
 alias ll='exa -l --group-directories-first'
 alias la='exa -la --group-directories-first'
 alias gd='cd $(git rev-parse --show-toplevel)'
+
+# Google Cloud SDK.
+if [ -f '/Users/l.alcaras/.google-cllooud-sdk/google-cloud-sdk/path.fish.inc' ]
+    . '/Users/l.alcaras/.google-cllooud-sdk/google-cloud-sdk/path.fish.inc'
+end
+
+# Python
+if [ -f '/Library/Frameworks/Python.framework/Versions/3.12/bin' ]
+    set -x PATH '/Library/Frameworks/Python.framework/Versions/3.12/bin' "$PATH"
+end
