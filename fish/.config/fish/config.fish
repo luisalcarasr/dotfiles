8310bb725675264e0ab4589c4bfe585b75e1578b
Luis Alcaras <l.alcaras@f5.com>
2025-09-06 20:48:02 -0700

    🐟 Some old aliases have been removed.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,14 +1,6 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
 set fish_greeting
-alias dotctl='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
 alias ls='exa --group-directories-first'
 alias ll='exa -l --group-directories-first'
 alias la='exa -la --group-directories-first'
-alias py=python
-alias htb='nmcli c up HTB && sudo route del -net default gw 10.10.14.1 netmask 0.0.0.0 dev tun0'
-alias gg=lazygit
-alias dot='gg --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-alias gr='cd $(git rev-parse --show-toplevel)'
-alias luarocks='luarocks --tree lua_modules'
+alias gd='cd $(git rev-parse --show-toplevel)'
