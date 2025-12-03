8acd259f7b49361302813740e1d7c97b226d932e
Luis Alcaras <l.alcaras@f5.com>
2025-12-02 18:22:20 -0600

    🐟 New aliases and zoxide initialization have been added.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,11 +1,22 @@
 set fish_greeting
 
+zoxide init fish | source
+
 # Aliases
 alias ls='eza --group-directories-first'
 alias ll='eza -l --group-directories-first'
 alias la='eza -la --group-directories-first'
 alias gd='cd $(git rev-parse --show-toplevel)'
 alias ai='ollama run gemma3:4b --'
+
+alias vim=nvim
+alias vi=vim
+
+alias py=python3
+alias python=python3
+alias pip=pip3
+
+alias cd=z
 
 # Google Cloud SDK.
 if [ -f '/Users/l.alcaras/.google-cllooud-sdk/google-cloud-sdk/path.fish.inc' ]
